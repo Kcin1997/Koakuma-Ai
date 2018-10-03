25a13cffe02b439682513331e0218e9dd868d851
Googie2149 <gc2149@gmail.com>
2018-10-02 23:38:55 -0400

    Error logging!

--- MinitoriCore/Modules/Discord Bots/Discord Bots.cs
+++ MinitoriCore/Modules/Discord Bots/Discord Bots.cs
@@ -29,82 +29,91 @@ namespace MinitoriCore.Modules.DiscordBots
 
         private async Task ChangeRoles(string remainder, ulong role, bool addRole = true)
         {
-            var args = remainder.Split(' ').Where(x => x.Length > 0).ToList();
-            string reason = "";
-            var users = new List<IGuildUser>();
-
-            int i = 0;
-            foreach (var s in new List<string>(args))
+            try
             {
-                var id = s.TrimStart('<').TrimStart('@').TrimStart('!').TrimEnd('>');
-                ulong temp;
-                if (ulong.TryParse(id, out temp))
+
+                var args = remainder.Split(' ').Where(x => x.Length > 0).ToList();
+                string reason = "";
+                var users = new List<IGuildUser>();
+
+                int i = 0;
+                foreach (var s in new List<string>(args))
                 {
-                    var user = Context.Guild.GetUser(temp);
-
-                    if (user != null && user.IsBot)
-                        users.Add(user);
-                    
-                    args.RemoveAt(i);
-                    i++;
-                }
-                else
-                    break;
-            }
-
-            if (users.Count() == 0)
-            {
-                await RespondAsync("You need to mention some bots for this to work!");
-                return;
-            }
-
-            string action = "";
-            
-            switch (role)
-            {
-                case 132106771975110656:
-                case 132106637614776320:
-                    if (addRole)
-                        action = "Mute";
-                    else
-                        action = "Unmute";
-                    break;
-                default:
-                    action = "Role changed";
-                    break;
-            }
-
-            reason = $"[ {action} by {Context.User.Username}#{Context.User.Discriminator} ] {string.Join(" ", args)}".Trim();
-            
-            //int mutedUsers = 0;
-            var r = Context.Guild.GetRole(role);
-            
-            //StringBuilder output = new StringBuilder();
-            //output.AppendLine($"Added `{r.Name}` to the following bots:");
-
-            var roledBots = new List<IGuildUser>();
-            var unroledBots = new List<IGuildUser>(); // for bots that are unaffected by the requested action
-
-            foreach (var u in users)
-            {
-                if (addRole)
-                {
-                    if (!u.RoleIds.Contains(r.Id))
+                    var id = s.TrimStart('<').TrimStart('@').TrimStart('!').TrimEnd('>');
+                    ulong temp;
+                    if (ulong.TryParse(id, out temp))
                     {
-                        if (r.Id == 132106637614776320) // Full mute
+                        var user = Context.Guild.GetUser(temp);
+
+                        if (user != null && user.IsBot)
+                            users.Add(user);
+
+                        args.RemoveAt(i);
+                        i++;
+                    }
+                    else
+                        break;
+                }
+
+                if (users.Count() == 0)
+                {
+                    await RespondAsync("You need to mention some bots for this to work!");
+                    return;
+                }
+
+                string action = "";
+
+                switch (role)
+                {
+                    case 132106771975110656:
+                    case 132106637614776320:
+                        if (addRole)
+                            action = "Mute";
+                        else
+                            action = "Unmute";
+                        break;
+                    default:
+                        action = "Role changed";
+                        break;
+                }
+
+                reason = $"[ {action} by {Context.User.Username}#{Context.User.Discriminator} ] {string.Join(" ", args)}".Trim();
+
+                //int mutedUsers = 0;
+                var r = Context.Guild.GetRole(role);
+
+                //StringBuilder output = new StringBuilder();
+                //output.AppendLine($"Added `{r.Name}` to the following bots:");
+
+                var roledBots = new List<IGuildUser>();
+                var unroledBots = new List<IGuildUser>(); // for bots that are unaffected by the requested action
+
+                foreach (var u in users)
+                {
+                    if (addRole)
+                    {
+                        if (!u.RoleIds.Contains(r.Id))
                         {
-                            if (u.RoleIds.Contains((ulong)132106771975110656))
+                            if (r.Id == 132106637614776320) // Full mute
                             {
-                                await u.RemoveRoleAsync(Context.Guild.GetRole(132106771975110656),
-                                    new RequestOptions() { AuditLogReason = $"[ Roleswap by {Context.User.Username}#{Context.User.Discriminator} ] {string.Join(" ", args)}".Trim() });
+                                if (u.RoleIds.Contains((ulong)132106771975110656))
+                                {
+                                    await u.RemoveRoleAsync(Context.Guild.GetRole(132106771975110656),
+                                        new RequestOptions() { AuditLogReason = $"[ Roleswap by {Context.User.Username}#{Context.User.Discriminator} ] {string.Join(" ", args)}".Trim() });
+                                }
+                                await u.AddRoleAsync(r, new RequestOptions() { AuditLogReason = reason });
+                                roledBots.Add(u);
                             }
-                            await u.AddRoleAsync(r, new RequestOptions() { AuditLogReason = reason });
-                            roledBots.Add(u);
-                        }
-                        else if (r.Id == 132106771975110656) // Non-testing mute
-                        {
-                            if (u.RoleIds.Contains((ulong)132106637614776320))
-                                unroledBots.Add(u);
+                            else if (r.Id == 132106771975110656) // Non-testing mute
+                            {
+                                if (u.RoleIds.Contains((ulong)132106637614776320))
+                                    unroledBots.Add(u);
+                                else
+                                {
+                                    await u.AddRoleAsync(r, new RequestOptions() { AuditLogReason = reason });
+                                    roledBots.Add(u);
+                                }
+                            }
                             else
                             {
                                 await u.AddRoleAsync(r, new RequestOptions() { AuditLogReason = reason });
@@ -112,70 +121,116 @@ namespace MinitoriCore.Modules.DiscordBots
                             }
                         }
                         else
+                            unroledBots.Add(u);
+                    }
+                    else
+                    {
+                        if (r.Id == 132106637614776320 ||
+                            r.Id == 132106771975110656)
                         {
-                            await u.AddRoleAsync(r, new RequestOptions() { AuditLogReason = reason });
+                            await u.RemoveRolesAsync(new List<IRole>() { Context.Guild.GetRole(132106637614776320), Context.Guild.GetRole(132106771975110656) },
+                                new RequestOptions() { AuditLogReason = reason });
                             roledBots.Add(u);
                         }
+                        else if (u.RoleIds.Contains(r.Id))
+                        {
+                            await u.RemoveRoleAsync(r, new RequestOptions() { AuditLogReason = reason });
+                            roledBots.Add(u);
+                        }
+                        else
+                            unroledBots.Add(u);
                     }
-                    else
-                        unroledBots.Add(u);
                 }
-                else
+
+                if (roledBots.Count() == 0)
                 {
-                    if (r.Id == 132106637614776320 ||
-                        r.Id == 132106771975110656)
-                    {
-                        await u.RemoveRolesAsync(new List<IRole>() { Context.Guild.GetRole(132106637614776320), Context.Guild.GetRole(132106771975110656) }, 
-                            new RequestOptions() { AuditLogReason = reason });
-                        roledBots.Add(u);
-                    }
-                    else if (u.RoleIds.Contains(r.Id))
-                    {
-                        await u.RemoveRoleAsync(r, new RequestOptions() { AuditLogReason = reason });
-                        roledBots.Add(u);
-                    }
-                    else
-                        unroledBots.Add(u);
+                    await RespondAsync("None of those mentioned were affected.");
+                    return;
                 }
-            }
 
-            if (roledBots.Count() == 0)
-            {
-                await RespondAsync("None of those mentioned were affected.");
-                return;
-            }
-
-            // I probably shouldn't care this much about formatting
-            StringBuilder output = new StringBuilder();
-
-            if (addRole)
-                output.Append($"Added `{r.Name}` to the following bot");
-            else
-                output.Append($"Removed `{r.Name}` from the following bot");
-            
-            if (roledBots.Count() > 1)
-                output.Append("s:\n");
-            else
-                output.Append(":\n");
-
-            output.AppendLine(string.Join(", ", roledBots.Select(x => $"**{x.Username}#{x.Discriminator}**")));
-
-            if (unroledBots.Count() > 0)
-            {
-                if (unroledBots.Count() > 1)
-                    output.Append("These bots ");
-                else
-                    output.Append("This bot ");
+                // I probably shouldn't care this much about formatting
+                StringBuilder output = new StringBuilder();
 
                 if (addRole)
-                    output.AppendLine("already had that role:");
+                    output.Append($"Added `{r.Name}` to the following bot");
                 else
-                    output.AppendLine("didn't have that role to start:");
+                    output.Append($"Removed `{r.Name}` from the following bot");
 
-                output.AppendLine(string.Join(", ", unroledBots.Select(x => $"**{x.Username}#{x.Discriminator}**")));
+                if (roledBots.Count() > 1)
+                    output.Append("s:\n");
+                else
+                    output.Append(":\n");
+
+                output.AppendLine(string.Join(", ", roledBots.Select(x => $"**{x.Username}#{x.Discriminator}**")));
+
+                if (unroledBots.Count() > 0)
+                {
+                    if (unroledBots.Count() > 1)
+                        output.Append("These bots ");
+                    else
+                        output.Append("This bot ");
+
+                    if (addRole)
+                        output.AppendLine("already had that role:");
+                    else
+                        output.AppendLine("didn't have that role to start:");
+
+                    output.AppendLine(string.Join(", ", unroledBots.Select(x => $"**{x.Username}#{x.Discriminator}**")));
+                }
+
+                await RespondAsync(output.ToString().Trim());
             }
+            catch (Exception ex)
+            {
+                string exMessage;
+                
+                if (ex != null)
+                {
+                    while (ex is AggregateException && ex.InnerException != null)
+                        ex = ex.InnerException;
+                    exMessage = $"{ex.Message}";
+                    if (exMessage != "Reconnect failed: HTTP/1.1 503 Service Unavailable")
+                        exMessage += $"\n{ex.StackTrace}";
+                }
+                else
+                    exMessage = null;
 
-            await RespondAsync(output.ToString().Trim());
+                string sourceName = ex.Source?.ToString();
+
+                string text;
+                if (ex.Message == null)
+                {
+                    text = exMessage ?? "";
+                    exMessage = null;
+                }
+                else
+                    text = ex.Message;
+
+                StringBuilder builder = new StringBuilder(text.Length + (sourceName?.Length ?? 0) + (exMessage?.Length ?? 0) + 5);
+                if (sourceName != null)
+                {
+                    builder.Append('[');
+                    builder.Append(sourceName);
+                    builder.Append("] ");
+                }
+                builder.Append($"[{DateTime.Now.ToString("d")} {DateTime.Now.ToString("T")}] ");
+                for (int i = 0; i < text.Length; i++)
+                {
+                    //Strip control chars
+                    char c = text[i];
+                    if (c == '\n' || !char.IsControl(c) || c != (char)8226)
+                        builder.Append(c);
+                }
+                if (exMessage != null)
+                {
+                    builder.Append(": ");
+                    builder.Append(exMessage);
+                }
+
+                text = builder.ToString();
+
+                await RespondAsync(text);
+            }
         }
 
         [Command("mute")]
