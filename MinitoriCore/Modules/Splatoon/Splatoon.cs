f229a222392be5aff966cbac2f5a59a224c23e58
Googie2149 <gc2149@gmail.com>
2019-05-07 23:53:01 -0400

    error checking

--- MinitoriCore/Modules/Splatoon/Splatoon.cs
+++ MinitoriCore/Modules/Splatoon/Splatoon.cs
@@ -18,7 +18,7 @@ using System.Net;
 
 namespace MinitoriCore.Modules.Splatoon
 {
-    [RequireGuild(568302640371335168)]
+    //[RequireGuild(568302640371335168)]
     public class Splatoon : MinitoriModule
     {
         // private RankedService rankedService;
@@ -39,45 +39,67 @@ namespace MinitoriCore.Modules.Splatoon
         [Priority(1000)]
         public async Task SelectMap()
         {
-            string stage = "";
-
-            if (!Directory.Exists("./Images/Splatoon/"))
+            try
             {
-                Directory.CreateDirectory("./Images/Splatoon/");
+                string stage = "";
+
+                if (!Directory.Exists("./Images/Splatoon/"))
+                {
+                    Directory.CreateDirectory("./Images/Splatoon/");
+                }
+
+                Random asdf = new Random(); // Todo: Implement better rng
+                int fileCount = Directory.GetFiles("./Images/Splatoon/", "*.png").Count();
+
+                if (fileCount == 0)
+                {
+                    await RespondAsync("Something went wrong and I have no maps in my list!");
+                    return;
+                }
+                else if (fileCount == 1)
+                    stage = Directory.GetFiles("./Images/Splatoon/", "*.png").FirstOrDefault();
+                else if (fileCount > 1)
+                    stage = Directory.GetFiles("./Images/Splatoon/", "*.png").ToList().OrderBy(x => asdf.Next()).FirstOrDefault();
+
+                using (FileStream file = new FileStream($"./Images/Splatoon/{stage}", FileMode.Open, FileAccess.Read))
+                {
+                    EmbedBuilder builder = new EmbedBuilder();
+
+                    builder.ImageUrl = $"attachment://{stage}";
+                    builder.Title = "ImageUrl Option Formatting Test";
+
+                    //await Context.Channel.SendFileAsync($"./Images/Splatoon/{stage}", embed: builder.Build());
+                    await Context.Channel.SendFileAsync(file, stage, text: "ImageUrl Option Formatting Test", embed: builder.Build());
+
+                    file.Position = 0;
+
+                    builder = new EmbedBuilder();
+
+                    builder.ThumbnailUrl = $"attachment://{stage}";
+                    builder.Title = "ThumbnailUrl Option Formatting Test";
+
+                    //await Context.Channel.SendFileAsync($"./Images/Splatoon/{stage}", embed: builder.Build());
+                    await Context.Channel.SendFileAsync(file, stage, text: "ThumbnailUrl Option Formatting Test", embed: builder.Build());
+                }
             }
-
-            Random asdf = new Random(); // Todo: Implement better rng
-            int fileCount = Directory.GetFiles("./Images/Splatoon/", "*.png").Count();
-
-            if (fileCount == 0)
+            catch (Exception ex)
             {
-                await RespondAsync("Something went wrong and I have no maps in my list!");
+                await RespondAsync($"There was an error uploading that file:\n{ex.Message}");
+                string exMessage;
+                if (ex != null)
+                {
+                    while (ex is AggregateException && ex.InnerException != null)
+                        ex = ex.InnerException;
+                    exMessage = $"{ex.Message}";
+                    if (exMessage != "Reconnect failed: HTTP/1.1 503 Service Unavailable")
+                        exMessage += $"\n{ex.StackTrace}";
+                }
+                else
+                    exMessage = null;
+
+                Console.WriteLine(exMessage);
+
                 return;
-            }
-            else if (fileCount == 1)
-                stage = Directory.GetFiles("./Images/Splatoon/", "*.png").FirstOrDefault();
-            else if (fileCount > 1)
-                stage = Directory.GetFiles("./Images/Splatoon/", "*.png").ToList().OrderBy(x => asdf.Next()).FirstOrDefault();
-
-            using (FileStream file = new FileStream($"./Images/Splatoon/{stage}", FileMode.Open, FileAccess.Read))
-            {
-                EmbedBuilder builder = new EmbedBuilder();
-
-                builder.ImageUrl = $"attachment://{stage}";
-                builder.Title = "ImageUrl Option Formatting Test";
-
-                //await Context.Channel.SendFileAsync($"./Images/Splatoon/{stage}", embed: builder.Build());
-                await Context.Channel.SendFileAsync(file, stage, text: "ImageUrl Option Formatting Test", embed: builder.Build());
-
-                file.Position = 0;
-
-                builder = new EmbedBuilder();
-
-                builder.ThumbnailUrl = $"attachment://{stage}";
-                builder.Title = "ThumbnailUrl Option Formatting Test";
-
-                //await Context.Channel.SendFileAsync($"./Images/Splatoon/{stage}", embed: builder.Build());
-                await Context.Channel.SendFileAsync(file, stage, text: "ThumbnailUrl Option Formatting Test", embed: builder.Build());
             }
         }
     }
