f86d5d7fa252e1d642880e381e343a14a6b4857b
Googie2149 <gc2149@gmail.com>
2020-12-04 14:30:52 -0500

    change some of the logging

--- MinitoriCore/Modules/Discord Bots/AccountGateService.cs
+++ MinitoriCore/Modules/Discord Bots/AccountGateService.cs
@@ -24,8 +24,6 @@ namespace MinitoriCore
         private IServiceProvider services;
         private Config config;
 
-        
-
         public async Task Install(IServiceProvider _services)
         {
             client = _services.GetService<DiscordSocketClient>();
@@ -33,6 +31,46 @@ namespace MinitoriCore
             services = _services;
 
             client.UserJoined += Client_UserJoined;
+        }
+
+        private string MoreDifferentFancyTime(DateTimeOffset creationDate)
+        {
+            var time = DateTimeOffset.UtcNow - creationDate;
+
+            //int years = 0;
+            //int months = 0;
+            int days = 0;
+            int hours = 0;
+            int minutes = 0;
+
+
+            StringBuilder date = new StringBuilder();
+            if (minutes + /*years + months +*/ days + hours > 0)
+            {
+                //if (time. > 0)
+                //    date.Append($"{years} {((years == 1) ? "year" : "years")} ");
+                //if (months > 0)
+                //    date.Append($"{months} {((months == 1) ? "month" : "months")} ");
+                //if (months + years < 1)
+                //{
+                    if (days > 0)
+                        date.Append($"{days} {((days == 1) ? "day" : "days")} ");
+                    if (days < 4)
+                    {
+                        if (hours > 0)
+                            date.Append($"{hours} {((hours == 1) ? "hour" : "hours")} ");
+                    }
+                    if (days < 3)
+                    {
+                        if (minutes > 0)
+                            date.Append($"{minutes} {((minutes == 1) ? "minute" : "minutes")} ");
+                    }
+                //}
+            }
+            else
+                date.Append("less than a minute ");
+
+            return date.ToString();
         }
 
         private async Task Client_UserJoined(SocketGuildUser user)
@@ -49,9 +87,9 @@ namespace MinitoriCore
             if (user.CreatedAt > DateTimeOffset.Now.AddDays(config.AgeGate * -1))
             {
                 await user.AddRoleAsync(user.Guild.GetRole(784226125408763954));
-                await ((SocketTextChannel)user.Guild.GetChannel(467192652463341578)).SendMessageAsync($"`[{DateTimeOffset.Now.ToString("HH:mm:ss")}]` Young account joined:\n" +
+                await ((SocketTextChannel)user.Guild.GetChannel(784491009249247253)).SendMessageAsync($"`[{DateTimeOffset.Now.ToLocalTime().ToString("HH:mm:ss")}]` Young account joined:\n" +
                     $"{user.Username}#{user.Discriminator} ({user.Id}) ({user.Mention})\n" +
-                    $"**Account created** `{user.CreatedAt.ToString("d")} {user.CreatedAt.ToString("T")}`");
+                    $"Created {MoreDifferentFancyTime(user.CreatedAt)} ago.");
             }
         }
     }
