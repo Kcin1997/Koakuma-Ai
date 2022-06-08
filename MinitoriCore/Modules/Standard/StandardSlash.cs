b12c9900a9a02429d29d85da061a2ff82ccbaff4
Nick <nbl42@nau.edu>
2022-06-08 00:01:28 -0700

    Koakuma's First Slash Commands

---
+++ MinitoriCore/Modules/Standard/StandardSlash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Net;
+using System.Threading.Tasks;
+
+namespace MinitoriCore.Modules.Standard
+{
+    public class StandardSlash
+    {
+        private DiscordSocketClient client;
+        private const long deco = 185220517773574144;
+
+    }
+}
--- MinitoriCore/Program.cs
+++ MinitoriCore/Program.cs
@@ -7,6 +7,7 @@ using Discord;
 using Discord.WebSocket;
 using Discord.Rest;
 using Discord.Commands;
+using Discord.Net;
 using System.IO;
 using Microsoft.Extensions.DependencyInjection;
 //using MinitoriCore.Modules.UptimeTracker;
@@ -24,6 +25,7 @@ namespace MinitoriCore
             new Program().RunAsync().GetAwaiter().GetResult();
 
         private const long guildID = 110373943822540800;
+        private readonly ulong deco = 185220517773574144;
         private DiscordSocketClient socketClient;
         private DiscordRestClient restClient;
         private Config config;
@@ -137,7 +139,8 @@ namespace MinitoriCore
             //});
 
             //await client.CurrentUser.ModifyAsync(x => x.Avatar = new Image(File.OpenRead("Minitori.png")));
-
+            socketClient.Ready += GuildCommandService;
+            socketClient.SlashCommandExecuted += SlashCommandHandler;
             await Task.Delay(-1);
         }
 
@@ -334,6 +337,49 @@ namespace MinitoriCore
                     }
                 }
             });
+        }
+
+        public async Task GuildCommandService()
+        {
+            // Let's build a guild command! We're going to need a guild so lets just put that in a variable.
+            var guild = socketClient.GetGuild(deco);
+
+            // Next, lets create our slash command builder. This is like the embed builder but for slash commands.
+            var guildCommand = new SlashCommandBuilder();
+
+            // Note: Names have to be all lowercase and match the regular expression ^[\w-]{3,32}$
+            guildCommand.WithName("first-command");
+
+            // Descriptions can have a max length of 100.
+            guildCommand.WithDescription("This is my first guild slash command!");
+
+            // Let's do our global command
+            var globalCommand = new SlashCommandBuilder();
+            globalCommand.WithName("first-global-command");
+            globalCommand.WithDescription("This is my first global slash command");
+
+            try
+            {
+                // Now that we have our builder, we can call the CreateApplicationCommandAsync method to make our slash command.
+                await guild.CreateApplicationCommandAsync(guildCommand.Build());
+
+                // With global commands we don't need the guild.
+                await socketClient.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+                // Using the ready event is a simple implementation for the sake of the example. Suitable for testing and development.
+                // For a production bot, it is recommended to only run the CreateGlobalApplicationCommandAsync() once for each command.
+            }
+            catch (ApplicationCommandException exception)
+            {
+                // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+
+                // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+                Console.WriteLine(exception.Errors);
+            }
+        }
+
+        private async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            await command.RespondAsync($"You executed {command.Data.Name}");
         }
 
         private Task Log(LogMessage msg)
